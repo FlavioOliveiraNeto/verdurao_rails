e224c8c25b3945e03614cd5e795ed4e6aba8e109
FlavioOliveiraNeto <oliveiraneto.flavio2001@gmail.com>
2025-02-04 00:46:28 -0300

    Primeiro commit

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,43 @@
+class Api::ProdutosController < ApplicationController
+    before_action :set_produto, only: [:show, :update, :destroy]
+  
+    def index
+      render json: Produto.all
+    end
+  
+    def show
+      render json: @produto
+    end
+  
+    def create
+      produto = Produto.new(produto_params)
+      if produto.save
+        render json: produto, status: :created
+      else
+        render json: produto.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @produto.update(produto_params)
+        render json: @produto
+      else
+        render json: @produto.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @produto.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_produto
+      @produto = Produto.find(params[:id])
+    end
+  
+    def produto_params
+      params.require(:produto).permit(:nome, :descricao, :preco, :estoque)
+    end
+end
